d7cc52ba417aeb911372ef2fa6f68a2c9f0918c6
BORIS <githubcream.soundwave2011@recursor.net>
2015-11-21 20:20:47 -0500

    Create win32urldownloadtofile.lisp

---
+++ win32urldownloadtofile.lisp
@@ -0,0 +1,24 @@
+;downloads an image from the web as file.png using win32
+
+(ql:quickload :cffi)
+(cffi:load-foreign-library "urlmon.dll")
+
+;define the function
+(cffi:defcfun ("URLDownloadToFileA" url-dl-to-file) :long
+	( pCaller		:pointer )
+	( szURL			:pointer )
+	( szFilename	:pointer )
+	( dwReserved	:long )
+	( lpfnCB		:pointer )
+)
+
+;create foreign strings
+(setq url (cffi:foreign-string-alloc "http://common-lisp.net/static/imgs/lisplogo.png"))
+(setq filename (cffi:foreign-string-alloc "file.png"))
+
+;call the function
+(url-dl-to-file (cffi:null-pointer) url filename 0 (cffi:null-pointer))
+
+;free strings
+(cffi:foreign-string-free url)
+(cffi:foreign-string-free filename)
